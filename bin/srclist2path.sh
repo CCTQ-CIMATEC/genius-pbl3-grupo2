91bc236a6b8acb6b03095f5c6f6f3bd3f72ede49
Gustavo <gustavosantiago1018@gmail.com>
2025-06-17 17:32:53 -0300

    Remove unused RTL files and link submodule RTL after update template configuration

--- bin/srclist2path.sh
+++ bin/srclist2path.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
+#echo $(find ../ -name proj.config)
+for path in $(find ../ -name proj.config); do 
+    declare "$(sed s/PROJNAME=// $path)"="$(dirname ${path})"
+    #echo  "RISCV_RV31IA=${RISCV_RV31IA}"
+    #echo  "RISCV_RV31IA=${RISCV_RV31IB}"
+done
 
 srclist2paths () {
     srclist=$1
-    base_path="`dirname ${srclist} | xargs dirname`"
-    #echo "list ${srclist}"
+    #base_path="`dirname ${srclist} | xargs dirname`"
+    #eval "echo list ${srclist}"
     #echo "src base $base_path"
-    for srcfile in `cat $srclist`; do
-        #echo "file ${srcfile}"
+    for srcfile in `eval "cat ${srclist}"`; do
+        srcfile=`eval "echo ${srcfile}"`
         if [[ "`basename ${srcfile}`" =~ ".srclist" ]] ; then
-            srclist2paths "${base_path}/${srcfile}"
+            srclist2paths "${srcfile}"
             #list="${list} $retval"
         else 
-            if [[ ! "${list}" =~ "${base_path}/${srcfile}" ]] ; then
+            if [[ ! "${list}" =~ "${srcfile}" ]] ; then
                 #echo "Skeeping ${base_path}/${srcfile} already included in file list!"
             #else
-                list="${list} ${base_path}/${srcfile}"
+                list="${list} ${srcfile}"
             fi
         fi    
     done
@@ -22,6 +28,8 @@ srclist2paths () {
     #retval=${list}
 }
 
+
+
 list=""
 srclist2paths $@
-echo ${list}
+echo "${list}"
